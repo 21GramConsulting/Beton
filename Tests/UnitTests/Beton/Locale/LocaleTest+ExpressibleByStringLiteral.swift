754fc7ac3986e440b65c58978745f32b0d2e2c07
Tamás Kálcza <calydk@gmail.com>
2022-03-03 15:04:04 +0100

    Added unit test for expressing Locale with string literal.

---
+++ Tests/UnitTests/Beton/Locale/LocaleTest+ExpressibleByStringLiteral.swift
@@ -0,0 +1,10 @@
+import Foundation
+import XCTest
+@testable import Beton
+
+class LocaleTest: XCTestCase {
+  func test_Locale_from_string_literal() {
+    let locale: Locale = "hu_HU"
+    XCTAssertEqual("hu_HU", locale.identifier)
+  }
+}
