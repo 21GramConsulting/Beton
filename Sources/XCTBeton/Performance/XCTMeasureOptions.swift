f398ab45cf76c7d0dff0c133b6b70144b3389236
Tamás Kálcza <calydk@gmail.com>
2022-03-03 12:29:38 +0100

    Added an explicit init.

--- Sources/XCTBeton/Performance/XCTMeasureOptions.swift
+++ Sources/XCTBeton/Performance/XCTMeasureOptions.swift
@@ -1,4 +1,7 @@
 import Beton
 import class XCTest.XCTMeasureOptions
 
-open class XCTMeasureOptions: XCTest.XCTMeasureOptions {}
+open class XCTMeasureOptions: XCTest.XCTMeasureOptions {
+  // TODO: Figure out why is this not visible when used as a library?
+  public override init() {}
+}
