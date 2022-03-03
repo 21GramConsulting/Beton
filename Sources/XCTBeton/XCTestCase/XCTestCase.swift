059592fe25d16357301d9019f1c184e6a532cd6a
Tamás Kálcza <calydk@gmail.com>
2022-03-03 14:08:18 +0100

    Fixed a strange compile error.

--- Sources/XCTBeton/XCTestCase/XCTestCase.swift
+++ Sources/XCTBeton/XCTestCase/XCTestCase.swift
@@ -1,11 +1,12 @@
 import Beton
 import class XCTest.XCTestCase
+import protocol XCTest.XCTMetric
 
 open class XCTestCase: XCTest.XCTestCase {
   internal var lastRunMetrics: [XCTMetric] = []
-}
 
-extension XCTest.XCTestCase {
-  open class var defaultMetrics:        [XCTMetric] { [XCTClockMetric()] }
-  open class var defaultMeasureOptions: XCTMeasureOptions { XCTMeasureOptions() }
+  open class override var defaultMetrics: [XCTest.XCTMetric] { [XCTClockMetric()] }
+
+  // Moved this here from an `XCTest.XCTestCase` extension, which caused a strange compilation error on the first run on a clean build.
+  open class override var defaultMeasureOptions: XCTMeasureOptions { XCTMeasureOptions() }
 }
