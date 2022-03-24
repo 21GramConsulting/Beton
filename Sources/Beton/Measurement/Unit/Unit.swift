849e44f832ea43ccec3bc1464bc3363684184e23
Tamás Kálcza <calydk@gmail.com>
2022-03-24 09:15:18 +0100

    Added a simple documentation to Unit.

--- Sources/Beton/Measurement/Unit/Unit.swift
+++ Sources/Beton/Measurement/Unit/Unit.swift
@@ -1,6 +1,10 @@
 import Foundation
 
+/// Represents a [Unit](https://developer.apple.com/documentation/foundation/unit) of measure with a ``default`` unit.
 public protocol Unit: Foundation.Unit {
   associatedtype U = Self
+  /// The default unit. For example
+  /// [`meters`](https://developer.apple.com/documentation/foundation/unitlength/1855995-meters) for
+  /// [`UnitLength`](https://developer.apple.com/documentation/foundation/unitlength).
   static var `default`: U { get }
 }
