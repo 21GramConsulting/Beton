da5056c6e156c3ef1c1776fe4ccb80e6478060bb
Tamás Kálcza <calydk@gmail.com>
2022-03-24 11:19:23 +0100

    Added a minimal documentation to ?! operator.

--- Sources/Beton/Optional/Optional+throwIfOptional.swift
+++ Sources/Beton/Optional/Optional+throwIfOptional.swift
@@ -1,8 +1,10 @@
 infix operator ?!
 
-public func ?!<Value, Error>(lhs: Optional<Value>, rhs: Error) throws -> Value where Error: Swift.Error {
-  switch lhs {
-  case .none: throw rhs
+/// Performs a `nil` checking operation, returning the wrapped value of an
+/// [`Optional`](https://developer.apple.com/documentation/swift/optional) instance or throwing the given error.
+public func ?!<Value, Error>(optional: Optional<Value>, error: Error) throws -> Value where Error: Swift.Error {
+  switch optional {
+  case .none: throw error
   case .some(let value): return value
   }
 }
