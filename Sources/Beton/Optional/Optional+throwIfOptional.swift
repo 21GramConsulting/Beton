fdfd856a8dc4ea4ac545dff54b20a86715bc5efe
Tamás Kálcza <calydk@gmail.com>
2022-03-24 11:34:16 +0100

    Improved documentation of ?! operator.

--- Sources/Beton/Optional/Optional+throwIfOptional.swift
+++ Sources/Beton/Optional/Optional+throwIfOptional.swift
@@ -2,6 +2,28 @@ infix operator ?!
 
 /// Performs a `nil` checking operation, returning the wrapped value of an
 /// [`Optional`](https://developer.apple.com/documentation/swift/optional) instance or throwing the given error.
+///
+/// The ``?!`` operator unwraps the left-hand side if it has a value, or it throws the right-hand side. The result of
+/// this operation will have the non-optional type of the left-hand side’s `Wrapped` type.
+///
+/// Unlike the `??` operator, this operator *does not* use short-circuit evaluation. For example:
+///
+/// ```swift
+/// struct GenericError: Error {}
+///
+/// var called = false
+/// func error() -> Error {
+///   called = true
+///   return GenericError()
+/// }
+///
+/// let goodNumber = try Int("100") ?! error()
+/// // goodNumber == 100
+/// // called == true
+/// ```
+/// - Parameters:
+///   - optional: An optional value.
+///   - error: The error to throw if the optional value is `nil`.
 public func ?!<Value, Error>(optional: Optional<Value>, error: Error) throws -> Value where Error: Swift.Error {
   switch optional {
   case .none: throw error
--- Tests/UnitTests/Beton/Optional/OptionalTest+throwIfOptional.swift
+++ Tests/UnitTests/Beton/Optional/OptionalTest+throwIfOptional.swift
@@ -4,17 +4,28 @@ import Beton
 
 extension OptionalTest {
   func test_throw_if_optional_returns_value() throws {
-    let url = try URL(string: "https://21gram.consulting") ?! Error.failure
+    let url = try URL(string: "https://21gram.consulting") ?! GenericError()
     XCTAssertEqual(url.description, "https://21gram.consulting")
   }
 
   func test_throw_if_optional_throws_given_error() throws {
-    XCTAssertThrowsError(try URL(string: "Not a URL") ?! Error.failure) { error in
-      XCTAssertEqual(error as? Error, Error.failure)
+    XCTAssertThrowsError(try URL(string: "Not a URL") ?! GenericError()) { error in
+      XCTAssertEqual(error as? GenericError, GenericError())
     }
+  }
+
+  func test_throw_if_optional_does_not_short_circuiting() throws {
+    var called = false
+    func error() -> Error {
+      called = true
+      return GenericError()
+    }
+
+    let goodNumber = try Int("100") ?! error()
+
+    XCTAssertEqual(goodNumber, 100)
+    XCTAssertTrue(called)
   }
 }
 
-fileprivate enum Error: Swift.Error {
-  case failure
-}
+fileprivate struct GenericError: Error, Equatable {}
