03063d4f09809511dd1cacab570d5800b004d694
Tamás Kálcza <calydk@gmail.com>
2022-03-29 12:56:31 +0200

    Documented metric identifiers.

--- Sources/XCTBeton/XCTestCase/XCTAssertMetric/XCTAssertDiskIdentifier.swift
+++ Sources/XCTBeton/XCTestCase/XCTAssertMetric/XCTAssertDiskIdentifier.swift
@@ -1,5 +1,12 @@
 import Beton
 
+/// Identifies the type of disk usage you want to make assertion for in a performance test.
+///
+/// ``XCTStorageMetric`` records the amount of data logically written to the disk in the block argument to
+/// ``XCTestCase/measure(metrics:block:)``.To make assertion to these statistics use any of the values of
+/// ``XCTAssertDiskIdentifier``.
+///
+/// - SeeAlso: https://developer.apple.com/documentation/xctest/xctstoragemetric
 public enum XCTAssertDiskIdentifier {
   case logicalWrites
 }
