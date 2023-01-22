c36847f5addf5277cf1b3879ace53091c11f462f
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2023-01-22 09:17:22 +0100

    Added docs

--- Sources/Beton/URL/URL+ExpressibleByStringLiteral.swift
+++ Sources/Beton/URL/URL+ExpressibleByStringLiteral.swift
@@ -1,6 +1,13 @@
 import Foundation
 
 extension URL: ExpressibleByStringLiteral {
+  /// A convenience initializer that allows you to express a
+  /// [`URL`](https://developer.apple.com/documentation/foundation/url) using a string literal.
+  ///
+  /// ```swift
+  /// let assignmentExample: URL = "https://www.21gram.consulting/en-US"
+  /// let (pageDownloadExample, _) = try await URLSession.shared.data(from: "https://www.21gram.consulting/en-US")
+  /// ```
   public init(stringLiteral value: String) {
     guard let url = URL(string: value) else {
       preconditionFailure("Couldn't initialize URL from literal as it resolved to nil.")
