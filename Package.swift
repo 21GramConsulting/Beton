610ec5f12b5aeef6a1ff9edc50506f86879a3918
Ádám László Rocska <adam-rocska@users.noreply.github.com>
2023-10-22 19:59:50 +0200

    typo

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "0.1.0"),
-    .package(url: "https://github.com/apple/swift-format.git", branch: "5,5"),
+    .package(url: "https://github.com/apple/swift-format.git", branch: "5.5"),
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
   ],
   targets: [
