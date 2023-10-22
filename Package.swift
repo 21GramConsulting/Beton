835105b8769cc118781867a978bd03a649ed1203
Ádám László Rocska <adam-rocska@users.noreply.github.com>
2023-10-22 21:49:06 +0200

    jazzy can lick my butt, SwiftDocC is the new king

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "0.1.0"),
     .package(url: "https://github.com/apple/swift-format.git", branch: "release/5.9"),
-    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
   ],
   targets: [
     .target(
