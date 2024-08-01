eba5ca0cf40abc9f03f2c0dc5333ac0769a03e1b
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2024-08-01 16:36:23 +0200

    ⏫ Dependency & Platform upgrade

--- Package.swift
+++ Package.swift
@@ -7,14 +7,17 @@ let package = Package(
   defaultLocalization: LanguageTag("en_US"),
   platforms: [
     .macOS(.v12),
-    .iOS(.v13),
+    .iOS(.v15),
+    .tvOS(.v15),
+    .watchOS(.v8),
+    .visionOS(.v1),
   ],
   products: [
     .library(name: "Beton", targets: ["Beton"]),
     .library(name: "XCTBeton", targets: ["XCTBeton"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "0.1.0"),
+    .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "1.0.0"),
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
   ],
   targets: [
